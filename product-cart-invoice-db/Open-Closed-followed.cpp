018c9e6de95876c3f9e0c437a4377090a0260a5c
kamalsharma7 <kamal04062000@gmail.com>
2026-04-12 17:19:28 +0530

    Open-closed rule followed

---
+++ product-cart-invoice-db/Open-Closed-followed.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class product{
+public:
+    string name;
+    int price;
+         
+    product(string name , int price){
+        this->name=name;
+        this->price =price;
+    }
+};
+
+class Cart{
+public:
+    vector<product*> cartlist;
+
+    void addproduct(product* product){
+        cartlist.push_back(product);
+        cout<<"product-"<<product->name<<" added sucessfully in cart"<<endl;
+    }
+    
+    void calculatetotalprice(){
+        int total_price =0;
+        for(auto it:cartlist){
+            total_price+=it->price;
+        }
+        cout<<"total price is-"<<total_price<<endl;
+    }
+};
+
+class InvoicePrinter{
+public:
+    
+    void invoicedao(Cart* cart){
+        for(int i=0;i<(cart->cartlist).size();i++){
+            cout<<"Invoice print for product - "<<(cart->cartlist)[i]->name<<" and price is -"<<(cart->cartlist)[i]->price<<endl;
+        }
+    }
+};
+
+class SaveDbDao{
+public:
+    
+    virtual void savedbdao(Cart* cart){
+        for(int i=0;i<(cart->cartlist).size();i++){
+            cout<<"Product is successfully saved in db-"<<(cart->cartlist)[i]->name << "and price- "<< (cart->cartlist)[i]->price<<endl;
+    }
+}
+};
+
+class SaveMongoDao : public SaveDbDao{
+  void savedbdao(Cart* cart) override {
+      for(int i=0;i<(cart->cartlist).size();i++){
+        cout<<"save in Mongo-db product -"<<(cart->cartlist)[i]->name<<endl;
+      }
+  }  
+};
+
+int main() {
+    
+    Cart cart1;
+    cart1.addproduct(new product("perfume",400));
+    cart1.addproduct(new product("dryer",1000));
+    cart1.addproduct(new product("bag",1500));
+    
+    cart1.calculatetotalprice();
+    
+    InvoicePrinter invoice;
+    invoice.invoicedao(&cart1);
+
+    SaveDbDao* dbdao = new SaveMongoDao();
+    dbdao->savedbdao(&cart1);
+    
+    return 0;
+}    
